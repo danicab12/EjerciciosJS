0eec13513578d486893857072afd086599e2e652
danicab12 <danicabelloduarte@gmail.com>
2022-10-06 00:09:32 +0200

    avance

--- js/app.js
+++ js/app.js
@@ -1,12 +1,56 @@
+
+//Ejercicio 1:
 document.getElementById="cuadro";
-document.getElementById="h1";
     document.body.style.backgroundColor = "aquamarine";
     document.body.style.fontFamily = "Arial";
     document.body.style.color = "red";
     cuadro.style.border = "3px solid";
-    h1.style.color = "blue";
 
     const cambioColor = setInterval(setColor, 30000);
+    
     function setColor() {
-        document.body.style.backgroundColor = document.body.style.backgroundColor == "yellow" ? "pink" : "yellow";
+        var fondo=Math.floor(Math.random() * 8);
+        switch (fondo) {
+            case 0:
+                document.body.style.backgroundColor = "black";
+                break;
+                case 1:
+                    document.body.style.backgroundColor = "orange";
+                    break;
+                    case 2:
+                        document.body.style.backgroundColor = "yellow";
+                        break;
+                        case 3:
+                            document.body.style.backgroundColor = "pink";
+                            break;
+                            case 4:
+                                document.body.style.backgroundColor = "green";
+                                break;
+                                case 5:
+                                    document.body.style.backgroundColor = "white";
+                                    break;
+                                    case 6:
+                                        document.body.style.backgroundColor = "purple";
+                                        break;
+                                        case 7:
+                                            document.body.style.backgroundColor = "blue";
+                                            break;
+            default:
+                break;
+        }
     }
+
+//Ejercicio 2:
+
+document.body.firstElementChild.style.color = "blue";
+
+var lista = document.body.getElementsByTagName("li");
+    
+for (let i = 0; i <= lista.length-1; i++) {
+    console.log(lista[i].innerHTML);
+    
+}
+
+var elementos = [4, 5, 6 ];
+elementos.push(4);
+console.log(elementos[0]);
